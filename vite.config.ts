dfb249a6847ada1da57ea7af89bb96679be0d835
Ritesh <riteshbhadani4369@gmail.com>
2025-08-19 18:27:21 +0530

    Setup backend API

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,13 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    proxy: {
+      '/api': {
+        target: 'https://news-hub-backend.vercel.app',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
   },
   plugins: [
     react(),
